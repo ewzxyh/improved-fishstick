6626b772dd4a83ea6aec0a6e15edae3525118123
ewzxyh <yoshidaenzo@hotmail.com>
2024-10-14 21:46:00 -0300

    add gtag

--- components/LandingPage/Form.tsx
+++ components/LandingPage/Form.tsx
@@ -83,42 +83,40 @@ const Form: React.FC = () => {
       alert('Por favor, insira um e-mail válido e confirme os termos.');
     }
   };
-
+        {/* Load analytics.js */}
+        <Script
+        async
+        src="https://www.google-analytics.com/analytics.js"
+        strategy="beforeInteractive"
+      />
+      {/* Initialize analytics.js and enable the linker plugin */}
+      <Script id="analytics-init" strategy="afterInteractive">
+        {`
+          ga('create', 'AW-16733205759', 'auto');
+          ga('require', 'linker');
+          ga('linker:autoLink', ['linhasuper2.com', 'pre-blackfriday.linhasuper2.com']);
+        `}
+      </Script>
+      {/* Load gtag.js */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16733205759"
+        strategy="afterInteractive"
+      />
+      {/* Configure gtag.js */}
+      <Script id="gtag-config" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16733205759', {
+            'linker': {
+              'domains': ['linhasuper2.com', 'pre-blackfriday.linhasuper2.com']
+            }
+          });
+        `}
+      </Script>
   return (
-    <>
-    {/* Load analytics.js */}
-    <Script
-  async
-  src="https://www.google-analytics.com/analytics.js"
-  strategy="beforeInteractive"
-/>
-{/* Initialize analytics.js and enable the linker plugin */}
-<Script id="analytics-init" strategy="afterInteractive">
-  {`
-    ga('create', 'AW-16733205759', 'auto');
-    ga('require', 'linker');
-    ga('linker:autoLink', ['linhasuper2.com', 'pre-blackfriday.linhasuper2.com']);
-  `}
-</Script>
-{/* Load gtag.js */}
-<Script
-  async
-  src="https://www.googletagmanager.com/gtag/js?id=AW-16733205759"
-  strategy="afterInteractive"
-/>
-{/* Configure gtag.js */}
-<Script id="gtag-config" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'AW-16733205759', {
-      'linker': {
-        'domains': ['linhasuper2.com', 'pre-blackfriday.linhasuper2.com']
-      }
-    });
-  `}
-</Script>
     isModalVisible && (
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#300808] bg-opacity-[85%]">
         <form onSubmit={handleSubmit} className="bg-[#FF0000] p-20 rounded-3xl shadow-md relative min-h-[400px]">
@@ -197,7 +195,6 @@ const Form: React.FC = () => {
         </form>
       </div>
     )
-    </>
   );
 };
 
